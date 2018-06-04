3a2c8431409cc491f8a8a7d17f112ab151fa8b4b
Ahmad Hobby <ahmad.hobby@gmail.com>
2018-06-04 19:05:19 -0400

    Initial Commit

---
+++ Word Effects - Part 2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects - Part 2
+//
+//  Created by Ahmad Hobby on 2018-06-04.
+//  Copyright © 2018 Ahmad Hobby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
