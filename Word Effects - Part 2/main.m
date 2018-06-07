f3edccb5e2f5cfa31adb0ceac974272a6317851f
Ahmad Hobby <ahmad.hobby@gmail.com>
2018-06-07 19:38:41 -0400

    completed assignment

--- Word Effects - Part 2/main.m
+++ Word Effects - Part 2/main.m
@@ -10,8 +10,50 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        while (YES) {
+            
+            // 255 unit long array of characters
+            char inputChars[255];
+            
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            // print as a c string
+            printf("Your string is %s\n", inputChars);
+            
+            // convert char array to an NSString object
+            NSString *inputString = [NSString stringWithUTF8String: inputChars];
+            
+            NSLog(@"Input was: %@", inputString);
+            
+            NSString *upperCase = inputString.uppercaseString;
+            NSLog(@"Upper case text is: %@", upperCase);
+            
+            NSString *lowerCase = inputString.lowercaseString;
+            NSLog(@"Lower case text is: %@", lowerCase);
+            
+            NSUInteger numberizedText = [inputString intValue];
+            NSLog(@"Numberized text is: %ld", numberizedText);
+            
+            NSString *canadianizedText = [NSString stringWithFormat:@"%@, eh?", inputString];
+            NSLog(@"Canadianized text is: %@", canadianizedText);
+            
+            BOOL textWithQuestionMark = [inputString containsString:@"?"];
+            BOOL textWithExclamationPoint = [inputString containsString:@"!"];
+            
+            if (textWithQuestionMark) {
+                NSLog(@"I don't know");
+            } else if (textWithExclamationPoint) {
+                NSLog(@"Whoa, calm down!");
+            }
+            
+            NSString *replacedString = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+            NSLog(@"%@", replacedString);
+            
+        }
+        
     }
     return 0;
 }
